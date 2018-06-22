8886a223547ce41c434d265b71ffbc5edf73f551
JENTLE L DUKES <jeduk1@morgan.edu>
2018-06-22 13:46:19 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -5,6 +5,7 @@ your_favorite_word = "sunkiss"
 
 puts "My favorite word is #{sunkiss}"
 
+
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
